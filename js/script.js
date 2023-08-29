67edce0ac02b42e7022c4ba8f1fa47cc9f1240b0
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-08-29 11:15:24 +0200

    aggiunta la funzione di modifica dei list-item

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         }
     }
 
-    showEmptyMessage(); // Mostra il messaggio iniziale
+    showEmptyMessage();
 
     text.on('keypress', function (event) {
         if (event.which === 13) {
@@ -28,25 +28,66 @@ $(document).ready(function () {
 
         li.find('.delete').click(function () {
             li.remove();
-            showEmptyMessage(); // Mostra il messaggio se la lista è vuota dopo la rimozione
+            showEmptyMessage();
+        });
+
+        li.find('.modify').click(function () {
+            let listItem = $(this).closest('li');
+            let textElement = listItem.find('.text');
+            let liText = textElement.text();
+            let originalText = liText;
+
+            let input = $('<input type="text" class="edit-input">').val(liText);
+            let cancelButton = $('<div class="btn btn-outline-secondary cancel"><i class="fa-solid fa-times"></i></div>');
+            let confirmButton = $('<div class="btn btn-outline-success confirm"><i class="fa-solid fa-check"></i></div>');
+
+            textElement.replaceWith(input);
+            listItem.find('.btn-group').empty().append(cancelButton, confirmButton);
+
+            input.focus();
+
+            cancelButton.click(function () {
+                input.replaceWith(textElement);
+                listItem.find('.btn-group').empty().append('<div class="btn btn-outline-warning modify"><i class="fa-solid fa-pencil"></i></div>', '<div class="btn btn-outline-danger delete"><i class="fa-solid fa-trash"></i></div>');
+                configureListItemEvents(listItem);
+            });
+
+            confirmButton.click(function () {
+                let newText = input.val().trim();
+                if (newText !== '') {
+                    textElement.text(newText);
+                }
+                input.replaceWith(textElement);
+                listItem.find('.btn-group').empty().append('<div class="btn btn-outline-warning modify"><i class="fa-solid fa-pencil"></i></div>', '<div class="btn btn-outline-danger delete"><i class="fa-solid fa-trash"></i></div>');
+                configureListItemEvents(listItem);
+            });
+
+            input.on('keypress', function (event) {
+                if (event.which === 13) {
+                    confirmButton.click();
+                }
+            });
         });
     }
 
     function addListItem(liText) {
-        liText = liText.trim(); // Rimuovi spazi vuoti da inizio e fine
+        liText = liText.trim();
         if (liText !== '') {
             let newLi = $('<li>').html(`
                 <div class="d-flex align-items-center">
-                    <span class="check">&check;</span> ${liText}
+                    <span class="check">&check;</span> <span class="text">${liText}</span>
                 </div>
-                <div class="btn btn-outline-danger delete"><i class="fa-solid fa-trash"></i></div>
+                <div class="btn-group">
+                    <div class="btn btn-outline-warning modify"><i class="fa-solid fa-pencil"></i></div>
+                    <div class="btn btn-outline-danger delete"><i class="fa-solid fa-trash"></i></div>
+                </div>
             `);
 
             list.append(newLi);
             text.val('');
 
             configureListItemEvents(newLi);
-            showEmptyMessage(); // Nascondi il messaggio iniziale dopo l'aggiunta di un elemento
+            showEmptyMessage();
         }
     }
 
@@ -55,13 +96,14 @@ $(document).ready(function () {
         addListItem(liText);
     });
 
-    // Aggiungi il gestore di eventi click per l'eliminazione degli elementi esistenti
     list.on('click', '.delete', function () {
         $(this).closest('li').remove();
-        showEmptyMessage(); // Mostra il messaggio se la lista è vuota dopo la rimozione
+        showEmptyMessage();
     });
 
     btnReset.click(function () {
         text.val('');
     });
+
+    configureListItemEvents(list.find('li'));
 });
